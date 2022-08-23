1dba758906f5b1d99bd00a7512765a4a6a4ed386
Rajguru Chaitanya <chaitanya_s_rajguru@whirlpool.com>
2022-08-23 09:34:08 +0530

    Initial commit of solver

---
+++ solver.py
@@ -0,0 +1,19 @@
+""" solver.py
+
+Solve the ball sort puzzle.
+
+Usage: #TODO
+
+Input file format: #TODO
+
+Created by Chaitanya Rajguru 23-Aug-2022
+"""
+
+import sys
+import os
+
+def main():
+    """Solve a given game"""
+
+if __name__ == '__main__':
+    main()
