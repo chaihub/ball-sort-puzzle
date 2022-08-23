66b187b64ca5c967a7eefff48813b60d48f1e5bd
Rajguru Chaitanya <chaitanya_s_rajguru@whirlpool.com>
2022-08-23 09:56:07 +0530

    Working

--- solver.py
+++ solver.py
@@ -12,8 +12,18 @@ Created by Chaitanya Rajguru 23-Aug-2022
 import sys
 import os
 
+class BallSortGame:
+    def __init__(self):
+        pass
+
+def report_results(game):
+    pass
+
 def main():
     """Solve a given game"""
+    this_game = BallSortGame()
+
+    report_results(this_game)
 
 if __name__ == '__main__':
     main()
